c0e630457651e8daf65bbcd0a18618c828847eb9
caresiop <98849178+caresiop@users.noreply.github.com>
2025-03-06 17:26:19 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,24 @@
 # config.py:
 
 config = {
-    'audacity': {
-        'app_path': '' # local path to 'Audacity' app
-    },
-    'local': {
-        'save_path': '' # local path to 'local_recordings' folder
-    },
-    'google': {
-        'credentials_path': '', # local path to 'credentials.json'
-        'parent': '' # Google Drive Parent folder
-    },
-    # Google Drive folder IDs
-    'good_stewards_service': {
-        'music_folder': '',
-        'sermon_folder': '',
-        'misc_folder': ''
-    },
-    'cross_seeds_service': {
-        'music_folder': '',
-        'sermon_folder': '',
-        'misc_folder': ''
-    }
+        'audacity': {
+            'app_path': ''              # local path to 'Audacity' app
+        },
+        'local': {
+            'save_path': ''             # local path to 'local_recordings' folder
+        },
+        'google': {
+            'credentials_path': '',     # local path to 'credentials.json'
+            'parent': ''                # Google Drive Parent folder ID
+        },
+        'good_stewards_service': {      # (https://drive.google.com/drive/folders/[ID found here])
+            'music_folder': '',
+            'sermon_folder': '',
+            'misc_folder': ''
+        },
+        'cross_seeds_service': {
+            'music_folder': '',
+            'sermon_folder': '',
+            'misc_folder': ''
+        }
 }
